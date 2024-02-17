ede1b5902bec566ed1db66027ce987384a17c210
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-02-17 22:59:14 +0100

    Simple invoice code using BigDecimal and LocalDate class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String item = "Trek shoes";
+        int quantity = 3;
+        double weight = 0.45;
+        BigDecimal pricePerPiece = BigDecimal.valueOf(1800);
+        LocalDate dateOfIssue = LocalDate.of(2022,4,7);
+        boolean isDiscounted = true;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        double totalWeight = quantity * weight;
+        System.out.println("Total shipment weight: "+ totalWeight+" kg");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
+        BigDecimal totalPrice =
+                pricePerPiece.multiply(BigDecimal.valueOf(quantity));
+
+        totalPrice = totalPrice.add(BigDecimal.valueOf(45));
+
+        System.out.println("Celková cena zásilky: "+ totalPrice+" Kč");
     }
 }
